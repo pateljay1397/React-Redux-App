859a2249f957b95960982d2c5657eba315bc58ce
Jay Patel <pateljaypj1397@gmail.com>
2020-10-08 14:32:44 -0400

    Multiple Reducers

---
+++ src/redux/iceCream/iceCreamReducer.jsx
@@ -0,0 +1,18 @@
+const { BUY_ICECREAM } = require("./iceCreamTypes")
+
+const initialState ={
+    numberOfIceCream: 20
+}
+
+const iceCreamReducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_ICECREAM: return{
+            ...state,
+            numberOfIceCream: state.numberOfIceCream - 1
+        }
+
+        default: return state
+    }
+}
+
+export default iceCreamReducer
